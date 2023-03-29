d79e020ddd198fe8974154d92dc46efced38ca3b
Adib Hanna <adibhanna@gmail.com>
2023-03-29 13:51:53 -0500

    wip

--- config.lua
+++ config.lua
@@ -1,13 +1,32 @@
+lvim.colorscheme = "gruvbox-material"
+lvim.log.level = "warn"
+vim.o.background = "dark"
+vim.g.gruvbox_material_background = "hard"
+lvim.builtin.alpha.active = true
+lvim.reload_config_on_save = true
+-- lvim.builtin.illuminate.active = false
+-- lvim.builtin.bufferline.active = false
+-- lvim.builtin.alpha.mode = "dashboard"
+-- lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+lvim.builtin.breadcrumbs.active = true
+lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.dap.active = true
+lvim.format_on_save.enabled = false
+lvim.builtin.lualine.sections.lualine_a = { "mode" }
+lvim.builtin.lualine.options.theme = "gruvbox-material"
+lvim.transparent_window = true
+lvim.builtin.terminal.active = false
+lvim.builtin.alpha.active = true
+
 reload "user.plugins"
 reload "user.options"
 reload "user.keymaps"
 reload "user.autocommands"
 reload "user.lsp"
-
 reload "user.treesitter"
 reload "user.copilot"
 reload "user.telescope"
-
 reload "user.fidget"
 reload "user.whichkey"
 
---
+++ ftplugin/go.lua
@@ -0,0 +1,29 @@
+local status_ok, which_key = pcall(require, "which-key")
+if not status_ok then
+  return
+end
+
+local opts = {
+  mode = "n", -- NORMAL mode
+  prefix = "<leader>",
+  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true, -- use `silent` when creating keymaps
+  noremap = true, -- use `noremap` when creating keymaps
+  nowait = true, -- use `nowait` when creating keymaps
+}
+
+local mappings = {
+  G = {
+    name = "+Go",
+    i = { "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies" },
+    t = { "<cmd>GoMod tidy<cr>", "Go Tidy" },
+    a = { "<cmd>GoTestAdd<Cr>", "Add Test" },
+    A = { "<cmd>GoTestsAll<Cr>", "Add All Tests" },
+    e = { "<cmd>GoTestsExp<Cr>", "Add Exported Tests" },
+    g = { "<cmd>GoGenerate<Cr>", "Go Generate" },
+    f = { "<cmd>GoGenerate %<Cr>", "Go Generate File" },
+    c = { "<cmd>GoCmt<Cr>", "Generate Comment" },
+  },
+}
+
+which_key.register(mappings, opts)
